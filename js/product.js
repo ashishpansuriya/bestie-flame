85707bafb3aa685d4305cfafefa1cd6461e47069
Rohit Khokhar <rohitkhokharrk123@gmail.com>
2025-12-18 17:32:49 +0530

    bug fixed

---
+++ js/product.js
@@ -0,0 +1,23 @@
+const products = [
+    {
+        id: 1,
+        name: "Luxury Chair",
+        price: 1500,
+        desc: "High quality wooden chair",
+        img: "img/product-img/product1.jpg"
+    },
+    {
+        id: 2,
+        name: "Office Chair",
+        price: 2200,
+        desc: "Ergonomic office chair",
+        img: "img/product-img/product2.jpg"
+    },
+    {
+        id: 3,
+        name: "Comfort Chair",
+        price: 1800,
+        desc: "Soft and comfortable",
+        img: "img/product-img/product3.jpg"
+    }
+];
