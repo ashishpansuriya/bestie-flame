5749e04306723e3177b22604816c1c6b12b93fbd
Rohit Khokhar <rohitkhokharrk123@gmail.com>
2025-12-25 12:49:56 +0530

    Products added

--- js/product.js
+++ js/product.js
@@ -1,23 +1,58 @@
 const products = [
     {
         id: 1,
-        name: "Luxury Chair",
-        price: 1500,
-        desc: "High quality wooden chair",
-        img: "img/product-img/product1.jpg"
+        name: "Love Flower Bookay",
+        price: 300,
+        desc: "Perfect gift for couple scented candle",
+        img: "img/product-img/bfs-1.jpeg"
     },
     {
         id: 2,
-        name: "Office Chair",
-        price: 2200,
-        desc: "Ergonomic office chair",
-        img: "img/product-img/product2.jpg"
+        name: "Rope Wax Jar Candle",
+        price: 149,
+        desc: "Perfect gift for couple scented candle (Pack of 2).",
+        img: "img/product-img/bfs-2.jpeg"
     },
     {
         id: 3,
-        name: "Comfort Chair",
-        price: 1800,
-        desc: "Soft and comfortable",
-        img: "img/product-img/product3.jpg"
+        name: "Rose Candle",
+        price: 149,
+        desc: "Perfect for cancdle light dinner scented candle",
+        img: "img/product-img/bfs-3.jpeg"
+    },
+    {
+        id: 4,
+        name: "Daisy flower candle",
+        price: 150,
+        desc: "Perfect gift for couple scented candle (Pack of 4). 100% soy wax | Floral scented | multi colour",
+        img: "img/product-img/bfs-4.jpeg"
+    },
+    {
+        id: 5,
+        name: "Tulip flower candle",
+        price: 170,
+        desc: "Perfect gift for couple scented candle (Pack of 4). Pure soy wax | Floral scented | multi colour",
+        img: "img/product-img/bfs-5.jpeg"
+    },
+    {
+        id: 6,
+        name: "Heart rose flower candle",
+        price: 149,
+        desc: "Perfect gift for couple scented candle (Pack of 2). 100% soy wax | Floral scented | multi colour",
+        img: "img/product-img/bfs-6.jpeg"
+    },
+    {
+        id: 7,
+        name: "Beutiful moon candle",
+        price: 250,
+        desc: "Perfect gift for couple honeymoon scented candle (Pack of 2).",
+        img: "img/product-img/bfs-7.jpeg"
+    },
+    {
+        id: 8,
+        name: "Small heart candle",
+        price: 169,
+        desc: "Perfect gift for couple scented candle (Pack of 4). 100% soy wax Multicolour heart",
+        img: "img/product-img/bfs-8.jpeg"
     }
 ];
